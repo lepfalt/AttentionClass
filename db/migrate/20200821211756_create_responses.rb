d7e7e28578b2f10d825074857e4fd5ea142878ed
leticia.pfaltzgraff <lepfalt@gmail.com>
2020-08-21 18:18:20 -0300

    Criaçao da resposta da tarefa.

---
+++ db/migrate/20200821211756_create_responses.rb
@@ -0,0 +1,15 @@
+class CreateResponses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :responses do |t|
+      t.integer :person_id
+      t.integer :task_id
+      t.text :response_value
+      t.text :annotation_person
+      t.integer :status
+      t.float :grade
+      t.text :observation_responsible
+
+      t.timestamps
+    end
+  end
+end
