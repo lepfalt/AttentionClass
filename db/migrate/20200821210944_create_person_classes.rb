4dc6fb4c720e216380c6ac1222fd6a0fb5571d10
leticia.pfaltzgraff <lepfalt@gmail.com>
2020-08-21 18:20:09 -0300

    Alteraçao das fks pra classe_id.

--- db/migrate/20200821210944_create_person_classes.rb
+++ db/migrate/20200821210944_create_person_classes.rb
@@ -1,8 +1,8 @@
 class CreatePersonClasses < ActiveRecord::Migration[6.0]
   def change
     create_table :person_classes do |t|
-      t.integer :person_code
-      t.integer :class_code
+      t.integer :person_id
+      t.integer :class_id
 
       t.timestamps
     end
--- db/migrate/20200821211225_create_tasks.rb
+++ db/migrate/20200821211225_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
       t.string :title
-      t.integer :class_code
+      t.integer :class_id
       t.text :description
       t.integer :status
       t.date :expiration_date
