56097297d49a0defb24e20e532022beadb2bef0f
leticia.pfaltzgraff <lepfalt@gmail.com>
2020-08-21 19:00:04 -0300

    criaçao da view login

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :responses
   resources :tasks
   resources :class_groups
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "login", to: "login#index"
 end
