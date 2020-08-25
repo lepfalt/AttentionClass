a920c2316cb0ffe4fb16d0403ccbb5467eae1843
leticia.pfaltzgraff <lepfalt@gmail.com>
2020-08-25 19:01:42 -0300

    Validaçoes nos models.

--- app/models/class_group.rb
+++ app/models/class_group.rb
@@ -1,5 +1,8 @@
 class ClassGroup < ApplicationRecord
   has_many :tasks
   has_and_belongs_to_many :users
-  belongs_to :user, optional: false
+  belongs_to :user, optional: false # Verificar depois
+
+  validates :discipline, presence: true
+  validates :expiration_date, presence: true
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -2,4 +2,9 @@ class Response < ApplicationRecord
   enum status: { pending: 0, progress: 1, done: 2, undelivered: 3 }
   belongs_to :user, optional: false
   belongs_to :task, optional: false
+
+  validates :status, :user_id, :task_id, presence: true
+  validates :status, inclusion: { in: [pending, progress, done, undelivered] }
+  validates_associated :user
+  validates_associated :task
 end
