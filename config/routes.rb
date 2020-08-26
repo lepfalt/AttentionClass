20fdef7be327563f2b4e30837d079388ed506592
leticia.pfaltzgraff <lepfalt@gmail.com>
2020-08-25 22:56:02 -0300

    Visualizaçao por perfil.

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,6 @@ Rails.application.routes.draw do
   resources :class_groups
   #root to: 'home#index'
   get "home", to: 'home#index'
-  # get "login", to: 'login#index'
   get "turmas", to: 'class_groups#index'
   #get "tarefas", to: 'tasks#index'
 
