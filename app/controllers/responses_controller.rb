6abe6a50e9e9bb22fa3fe78f4084dccd3f2ed6c7
leticia.pfaltzgraff <lepfalt@gmail.com>
2020-08-27 19:32:06 -0300

    Visualizaçao dos responses do aluno

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,5 +1,5 @@
 class ResponsesController < ApplicationController
-  #before_action :set_response, only: %i[show edit update destroy]
+  before_action :set_response, only: %i[show edit update destroy]
 
   # GET /responses
   # GET /responses.json
