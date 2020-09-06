157108a127dedc829cca87a7152b958a79ee5e3e
leticia.pfaltzgraff <lepfalt@gmail.com>
2020-09-05 23:04:30 -0300

    Reformulando tela de login.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def get_class_name(class_name)
+    current_page?(login_path) ? "" : class_name
+  end
+
+  def get_main_class()
+    current_page?(login_path) ? "main-content-login" : "main-content"
+  end
 end
