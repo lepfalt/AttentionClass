c52aa17db51835d2063f1b82d3f90b794ffd0824
leticia.pfaltzgraff <lepfalt@gmail.com>
2020-10-17 16:29:04 -0300

    Ajuste no front da root.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
 
   def get_class_name(class_name)
-    current_page?(login_path) ? "" : class_name
+    current_page?(login_path) || current_page?(root_path) ? "" : class_name
   end
 
   def get_main_class()
-    current_page?(login_path) ? "main-content-login" : "main-content"
+    current_page?(login_path) || current_page?(root_path) ? "main-content-login" : "main-content"
   end
 end
